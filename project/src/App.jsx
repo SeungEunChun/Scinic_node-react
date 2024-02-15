048c18a024feb5691de2373f8f0bbea4140f5dd5
SeungEunChun <1000tmddms@gmail.com>
2024-02-15 22:25:25 +0900

    axios 요청을 과하게 하던것 코드 최적화/자식 컴포넌트에게 보내줄때 &&식으로 한번 더 검수하면서 데이터가 확실하게 올수 있도록 조정

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import * as React from 'react';
+import { useEffect, useState } from 'react';
 import { Routes, Route } from "react-router-dom";
 import axios from 'axios';
 import Header from "./Components/Header";
@@ -23,103 +24,70 @@ import Store from './Link/Store';
 
 import review from './data/review.json'
 import award from "./data/award.json"
-import Cookies from 'js-cookie';
+
 
 
 
 function App() {
-  const [totalpro, settotal] = useState([]);
-  const [catepro, setcate] = useState([]);
-
+  const [totalpro, settotal] = useState({});
 
 
   useEffect(() => {
-    const fetchcookie = async () => {
-      const savetotal = Cookies.get('totalpro');
-      const savecate = Cookies.get('catepro');
-      if (savetotal && savecate) {
-        settotal(JSON.parse(savetotal));
-        setcate(JSON.parse(savecate));
-      } else {
-        await dbstore("store", "Scinic_Product");
-        await category_no();
-        Cookies.set('totalpro', JSON.stringify(totalpro), { expires: 7, path: "/" })
-        Cookies.set('catepro', JSON.stringify(catepro), { expires: 7, path: "/" })
-      }
-
-    }
-    fetchcookie();
-    const dbstore = async (r, t, cate = "all") => {
+    const dbstore = async (r, data = null) => {
+      const rarry = r.split('/');
+      const tn = rarry[1]; //게시판 이름
       try {
-        if (cate !== "all") {
-          const result = await axios.get(`/${r}/${t}/${cate}`, {
+        if (data) {
+          const result = await axios.post(`/${r}`, {
             headers: {
-              'Content-Type': 'application/json'
+              'Content-Type': 'multipart/form-data'
             },
-            params: {
-              param: "아무거나"
-            }
+            params: data
           });
-          setcate([...result.data])
+          settotal(prevState => ({
+            ...prevState,
+            [tn]: [...result.data]
+          }))
         } else {
-          const result = await axios.get(`/${r}/${t}`, {
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            params: {
-              param: "아무거나"
-            }
-          });
-          settotal([...result.data])
+          const result = await axios.get(`/${r}`);
+          settotal(prevState => ({
+            ...prevState,
+            [tn]: [...result.data]
+          }))
         }
-
-
-        ;
-      } catch (error) {
-        console.log(error);
+      } catch (err) {
+        console.log(err);
       }
     };
 
-    const category_no = async () => {
-      try {
+    dbstore('store/Scinic_Product');
+    dbstore('store/Category');
 
-        const catename = await axios.get('/store/Category', {
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          params: {
-            param: "아무거나"
-          }
-        });
-        setcate([...catename.data])
-
-
-
-          ;
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    dbstore("store", "Scinic_Product");
-    category_no();
 
   }, []);
+
+  useEffect(() => {
+    console.log(totalpro); //for dev
+  }, [totalpro])
+
+
+
 
 
   return (
     <>
-      <Header datasrc={catepro && catepro}></Header>
+      <Header datasrc={totalpro && totalpro['Category'] && totalpro['Category']}></Header>
       <Routes>
         <Route path="/" element={<section className='mainsec'>
           <Mainswiper></Mainswiper>
-          <Sproduct datasrc={totalpro && totalpro}></Sproduct>
+          <Sproduct datasrc={totalpro && totalpro['Scinic_Product'] && totalpro['Scinic_Product']}></Sproduct>
 
           <div className="mb-5">
             <Scrollimg></Scrollimg>
             <Scrollimg2></Scrollimg2>
           </div>
           <Youtubev></Youtubev>
-          <Product datasrc={totalpro && totalpro} catesrc={catepro && catepro}></Product>
+          <Product datasrc={totalpro && totalpro['Scinic_Product'] && totalpro['Scinic_Product']} catesrc={totalpro && totalpro['Category'] && totalpro['Category']}></Product>
           <Mbenefit></Mbenefit>
           <Reviews datasrc={review.review}></Reviews>
           <Awards datasrc={award.award}></Awards>
@@ -127,7 +95,7 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/brand" element={<Brandstory />}></Route>
         <Route path='/promotion' element={<Promotion />}></Route>
-        <Route path='/store/:Category_no' element={<Store datasrc={totalpro && totalpro} catesrc={catepro && catepro} />}></Route>
+        <Route path='/store/:Category_no' element={<Store datasrc={totalpro && totalpro['Scinic_Product'] && totalpro['Scinic_Product']} catesrc={totalpro && totalpro['Category'] && totalpro['Category']} />}></Route>
 
       </Routes>
       <Footer></Footer>
